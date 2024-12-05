c3c7c2811ffd7111018b42fe3db9702ea012021d
arnica-engineering <95713264+arnica-engineering@users.noreply.github.com>
2024-12-05 15:05:13 -0500

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,10 @@
+
+## The credentials below are real. Please do not use them for malicious purposes.
+
+import tqdm 
+
+a = 'AKIA5677P7BCP2ZNJQ5Y' 
+b = f'visit https://gitgoat.tools'
+c = 'dFeVG+KhqTlH7nmdmbJHMsh8vE9jA6uoe/S406wR'
+
+print(b)
